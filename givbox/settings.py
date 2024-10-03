d9d40d37b081c23c2285952ed8978e4020abacfc
Yerassyliitu <34080@iitu.edu.kz>
2024-10-03 19:31:52 +0500

    fix: swagger cors error 1.1

--- givbox/settings.py
+++ givbox/settings.py
@@ -298,7 +298,7 @@ SIMPLE_JWT = {
     "SLIDING_TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSlidingSerializer",
 }
 
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ORIGIN_WHITELIST = (
 #    'http://localhost:3000',
 #    'http://localhost:5173',
